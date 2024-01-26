a456f9b1fb862e7d5a55a98f8dfc732bee931ed3
test <kmj020901@naver.com>
2024-01-26 19:55:46 +0900

    commit

---
+++ src/main/java/com/oggiraffers/literal/application1.java
@@ -0,0 +1,13 @@
+package com.oggiraffers.literal;
+
+public class application1 {
+
+    public static void main(String[] args) {
+        System.out.println(1+1); // 2
+    }
+
+    //메모
+    /* 메모 ㅁㄴㅇㄴㅇㄴㅇㄴㅇㅁㄴㅇㅁㄴㅇㅁ */
+
+
+}
